a9b0bcb8f162cc6515e4aa46eab83e43ff1b8b56
Alexis von Glasow <vonglasow@gmail.com>
2018-11-02 11:41:41 +0100

    debugging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+// Derive the `fmt::Debug` implementation for `Structure`. `Structure`
+// is a structure which contains a single `i32`.
+#[derive(Debug)]
+struct Structure(i32);
+
+// Put a `Structure` inside of the structure `Deep`. Make it printable
+// also.
+#[derive(Debug)]
+struct Deep(Structure);
+
 fn main() {
-    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+    // Printing with `{:?}` is similar to with `{}`.
+    println!("{:?} months in a year.", 12);
+    println!("{1:?} {0:?} is the {actor:?} name.",
+             "Slater",
+             "Christian",
+             actor="actor's");
+
+    // `Structure` is printable!
+    println!("Now {:?} will print!", Structure(3));
+
+    // The problem with `derive` is there is no control over how
+    // the results look. What if I want this to just show a `7`?
+    println!("Now {:?} will print!", Deep(Structure(7)));
+
+    let z: i32 = 3;
+    let y: i32 = 2;
+    let x: f32 = z % y;
+
+    println!("{:32}", x);
 }
